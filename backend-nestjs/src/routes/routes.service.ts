01d8e90088ca24ff44d27458be6e1ad2d91ad485
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-21 16:02:49 -0300

    create route resource module

---
+++ backend-nestjs/src/routes/routes.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class RoutesService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createRouteDto: CreateRouteDto) {
+    return this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: {
+          name: createRouteDto.source_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        destination: {
+          name: createRouteDto.destination_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        distance: 0,
+        duration: 0,
+        directions: '{}',
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.route.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
